aa9934a7444431db9d269c468376aae2a1fa962b
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-11 16:07:22 +0530

    graphql Server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { buildSchema } = require('Graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = buildSchema(`
+type Query {
+    description:String
+    price:Float
+}
+
+`);
+
+const root = {
+    description: 'Red Shoe',
+    price: 42.12,
+}
+
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+
+app.listen(3000, () => {
+    console.log('Running Graphql server...')
+})
